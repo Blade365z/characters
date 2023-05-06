fe8060f4a12fd9085ad1e21532a703116fff49fe
blade365z <amitabhyo@gmail.com>
2023-05-06 14:01:23 +0530

    added page params

--- src/services.ts
+++ src/services.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { Character } from "./types";
 
-export const fetchCharacters = async (): Promise<Character[]> => {
+export const fetchCharacters = async ({ page = 0 }): Promise<Character[]> => {
   const response = await axios.get(
-    "https://rickandmortyapi.com/api/character/?page=1"
+    `https://rickandmortyapi.com/api/character/?page=${page}`
   );
   return response?.data?.results || [];
 };
