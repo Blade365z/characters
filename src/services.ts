ab4bf84f769ff0a8548dfe2342f10e010033cbe7
blade365z <amitabhyo@gmail.com>
2023-05-06 13:56:14 +0530

    add pagination params

--- src/services.ts
+++ src/services.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import { Character } from "./types";
 
 export const fetchCharacters = async (): Promise<Character[]> => {
-  const response = await axios.get("https://rickandmortyapi.com/api/character");
+  const response = await axios.get(
+    "https://rickandmortyapi.com/api/character/?page=1"
+  );
   return response?.data?.results || [];
 };
